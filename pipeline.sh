336ce6ea97a72e9f1d2752579644fb99c801cc1b
= <=>
2023-05-21 01:08:42 +0800

    basic pipeline script for deploying argocd

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,21 @@
 #!/bin/sh -ux
+# All user-defined names will start with the generic prefix "xd-", followed by the application name
 
 deploy_argocd() {
+  namespace="argocd"
   echo deploying application argocd in namespace argocd
-  helm repo add argocd helm-charts/argocd
-  helm install argocd argocd
 
+  helm repo update
+  helm repo add argo https://argoproj.github.io/argo-helm
+  helm upgrade --install xd-argocd argo/argo-cd \
+    -f helm-charts/argo-cd/ci/ha-autoscaling-values.yaml \
+    --atomic \
+    --timeout 10m \
+    --wait \
+    --namespace "${namespace:?}" \
+    --create-namespace \
+    --debug
 }
+
+
+deploy_argocd
